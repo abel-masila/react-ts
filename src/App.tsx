d3e29c7e0a756a31f0b4347b3432e90f79ccd75d
Abel Masila <abelmasila@gmail.com>
2020-07-20 20:19:39 +0300

    tuples

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,15 @@ function App() {
   //let secondValue: number = 2;
   //let secondValue: boolean = true;
   //let secondValue: number[] = [2, 3, 4];
-  let secondValue: Array<number> = [2, 3, 4];
+  //let secondValue: Array<number> = [2, 3, 4];
+  let aTuple: [string, number] = ["1", 2];
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The value {secondValue} is of {typeof secondValue} type
+          The value {aTuple[1]} is of {typeof aTuple[1]} type
         </p>
       </header>
     </div>
