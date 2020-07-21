eecc148b30feccc1e5863fa00843b237ca79a75b
Abel Masila <abelmasila@gmail.com>
2020-07-21 18:20:35 +0300

    functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
-  //let firstValue: string = "Many";
-  //let secondValue: number = 2;
-  //let secondValue: boolean = true;
-  //let secondValue: number[] = [2, 3, 4];
-  //let secondValue: Array<number> = [2, 3, 4];
-  let aTuple: [string, number] = ["1", 2];
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The value {aTuple[1]} is of {typeof aTuple[1]} type
-        </p>
+        <Message msg="This is a simple text" />
       </header>
     </div>
   );
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = (props: { msg: string }): any => {
+  return <p>{props.msg}</p>;
+};
+
+export default Message;
